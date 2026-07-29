ce5454260ff67c87ceba24b1f1f386cd26ef7933
Yamuna N <sit24ad063@sairamtap.edu.in>
2026-07-29 14:18:21 +0530

    27/07/2026

---
+++ DAY 1/CLASSWORK/LeapYear.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        if(year % 400 == 0) {
+
+            System.out.println("Leap Year");
+
+        }
+        else if(year % 100 == 0) {
+
+            System.out.println("Not Leap Year");
+
+        }
+        else if(year % 4 == 0) {
+
+            System.out.println("Leap Year");
+
+        }
+        else {
+
+            System.out.println("Not Leap Year");
+
+        }
+
+    }
+}
